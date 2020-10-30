0960213ed6fd689b0534f291c58b230286df7871
SynCap <syncap@ya.ru>
2020-10-30 20:09:55 +0800

    +InComment Help

--- gg.ps1
+++ gg.ps1
@@ -1,3 +1,18 @@
+<#
+.Description
+
+	Clone remote Git project to specified dir in shallow manner (with
+	--depth=1), then show README files, then install NPMs, and start it if
+	you're ask for that. Whants to be a friend for JS people :)
+
+.Example
+	gg -e -i -r test,build -m npm
+
+.Link
+	https://github.com/SynCap/get-git.git
+
+#>
+
 Param (
 	# Source repository URL
 	[Parameter (Position=0)]
@@ -5,12 +20,10 @@ Param (
 	[ValidatePattern("^(git@|https:).*\.git/?$")]
 	[Alias('Source')]
 	[string] $Url,
-
 	# Destination directory name
 	[Alias('Dest','o')]
 	[Parameter(Position=1)]
 	[string] $DestDir,
-
 	# Show usage information, describes parameters and swithches
 	[Alias('h','Help')]  [Switch] $ShowUsage,
 	# Turn off depth limitations. See `git help clone --depth`
@@ -24,19 +37,16 @@ Param (
 	# GG automatically opens README files from cloned project. This switch turns
 	# off this behovoir
 	[Alias('n')]         [Switch] $NoReadme,
-
 	# Specify Node package manager to use for install and/or start the scripts.
 	# Yarn specified by default. No checking for installed managers is provided.
 	[Alias('m','Mgr')]
     [ValidateSet('Yarn','NPM')]
     [String] $PackageManager = "yarn",
-
     # If `package.json` in cloned project and `scripts` are specified in it the
     # GG can launch them. To do this specify all needed to launch scripts in
     # order to be launched.
 	[Alias('r','Run','Script')]
 	[String[]] $RunScripts,
-
 	# Some projects can contains tons of README.md, README.txt, and so on. To
 	# limit number of files that can be opened this parameter is.
 	[int] $MaxReadmes = 3,
@@ -97,53 +107,38 @@ Param (
 ###################################### Functions
 
 function Finish ([int]$ExitCode = 0) {
-
 	################################### DEBUG
-
-
 	################################### Real FINISH
-
-	cd $StartDir
-	$RST
-	Exit $ExitCode
+		cd $StartDir
+		$RST
+		Exit $ExitCode
 }
 
 function Show-Usage {
 	"$GRN`nClone remote Git project to specified dir in shallow manner,"
 	"then show$YLW README$GRN files, then install$YLW NPMs$GRN, and start it"
 	"if you're ask for that. Whants to be a friend for JS people$YLW_ :)$GRN"
-
 	"`nUsage: $YLW$((Get-Item $PSCommandPath).Basename)$WHT <git_repo_url>$GRN [dest_dir] [options]"
-
 	"`nSamples of valid$WHT git_repo_url$($GRN)s and source code links:`n"
-
 	"  https://github.com/SynCap/get-git.git"
 	"  git@github.com:SynCap/get-git.git"
-
 	"`nOptions:"
-
 	"`n  -InstallPackages,"
 	"  -i  install NPMs if$WHT package.json$GRN exists"
-
 	"`n  -RunScript, -Run,"
 	"  -r  run$YLW npm run$WHT command$GRN if it present in$YLW package.json$GRN"
 	"      you may launch several commands sequentally:$YLW -Run build,start$grn"
-
 	"`n  -PackageManager,"
 	"  -m  specify package manager to use:$WHT yarn$grn,$wht npm$grn"
-
 	"`n  -EraseExisting,"
 	"  -e $wht Erase$GRN target folder if exists"
 	"`n  -NoReadme,"
 	"  -n $wht NO README$GRN will be shown but found"
-
 	"`n  -DeepCopy,"
 	"  -d $WHT DEEP$GRN copy, i.e. no$YLW --depth=1$GRN param $RST`n"
-
 	"$YLW`nExample:$CYN"
 	"$WHT  $ggName$CYN_ https://github.com/SynCap/get-git.git$wht -NoReadme$CYN ``"
 	"      '~Get The GIT'$wht -e -i -r$dgy test,build$wht -m$dgy npm$RST`n"
-
 	"$YLW  1.$GRN URL must be placed before destination dir name"
 	"$YLW  2.$GRN New dir name may be omitted"
 	"$YLW  3.$GRN The placements of the other switches does not matter$RST"
@@ -170,7 +165,6 @@ function Clone-Repo {
 	"`n$RED■$YLW_ $NewDir$RST"
 	draw (hr `') DarkYellow
 	$RST
-
 	# Collect all params to launch clone job
 	$GitPath = 'git' # (Get-Command git).Source
 	$GitRunParams = @(
@@ -182,26 +176,21 @@ function Clone-Repo {
 	}
 	$GitRunParams += '--'
 	$GitRunParams += $Url
-
 	if ($DestDir) {
 		$GitRunParams += $DestDir -Match ' ' ?
 			"`"$DestDir`"" :
 			$DestDir
 	}
-
 	# Just shows command line as how it may composed manually
 	$GitRunCmd = $GitPath,($GitRunParams -join ' ') -join ' '
 	"$GitRunCmd`n"
-
 	# Really launches the cloning
 	& $GitPath $GitRunParams
-
 	draw (hr `' $HrLength),`n DarkYellow
 }
 
 function Open-Readmes {
 	"`n$RED■$YLW_ README files$RST"
-
 	$readmeFiles = Get-ChildItem "readme*" -Recurse -Depth $MaxReadmeSearchDepth | select FullName -First $MaxReadmes
 	$readmeFiles | % {
 		draw $_.FullName,`n DarkCyan;
@@ -209,14 +198,12 @@ function Open-Readmes {
 			& $_.FullName
 		}
 	}
-
 	draw (hr `'),`n DarkYellow
 }
 
 function Show-GitLog {
 	$dateFormat = '%d.%m.%Y %H:%M:%S'
 	$prettyString = '%C(auto)%h%d %C(bold blue)%an %Cgreen%ad  %Creset%s'
-
 	git log --graph "--date=format:$dateFormat" "--pretty=format:$prettyString" *
 }
 
@@ -242,29 +229,23 @@ if ( Test-Path -LiteralPath "$NewDir" ) {
 	pushd $NewDir
 	Show-GitLog
 	Open-Readmes
-
 	if (Test-Path -LiteralPath 'package.json') {
 		"Found$YLW package.json$RST"
 	}
 }
 
 if (Test-Path 'package.json') {
-
 	if ($InstallPackages) {
 		"`nInstallation was requested. Prepare:$WHT $PackageManager$YLW $($Launch[$PackageManager].Install)$RST"
 		& $PackageManager $($Launch[$PackageManager].Install)
 		Write-Host (hr `'),"`n" -Foreground DarkYellow
 	}
-
 	if ($RunScripts.Count) {
 		"Ordered to launch the$wht $($RunScripts.Count)$rst scripts."
-
 		$PkgScripts = (Get-Content 'package.json' | ConvertFrom-Json -AsHashtable).scripts
-
 		$RunScripts.ForEach({
 			"`n$RED■$WHT $PackageManager$YLW $($Launch[$PackageManager].Run -f $_)$RST"
 			"$YLW$(hr `')$RST"
-
 			if (!$PkgScripts[$_]) {
 				"$YLW_RED $_$WHT_RED not found in$YLW_RED package.json $RST"
 			} else {
